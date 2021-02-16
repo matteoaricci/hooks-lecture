2dd69fe6e97b2cfc729aac84e81b88dd5d43993f
matteoaricci <matteo.a.ricci@gmail.com>
2021-02-16 15:54:19 -0500

    Finished the lecture

--- src/App.js
+++ src/App.js
@@ -4,12 +4,32 @@ import SongCardContainer from "./containers/SongCardContainer";
 import NewSongForm from "./Forms/NewSongForm";
 
 function App() {
-  
+  const [songs, setSongs] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/songs")
+      .then((resp) => resp.json())
+      .then((s) => setSongs(s));
+  }, []);
+
+  const submitSong = (song) => {
+    fetch("http://localhost:3000/songs", {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({
+        title: song.title,
+        album: song.album,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((song) => setSongs([...songs, song]));
+  };
+
   return (
     <div className="App">
       <Navbar />
-      <SongCardContainer  />
-      <NewSongForm  />
+      <SongCardContainer songs={songs} />
+      <NewSongForm submitSong={submitSong} />
     </div>
   );
 }
--- src/Forms/NewSongForm.jsx
+++ src/Forms/NewSongForm.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 import { useForm, Controller } from "react-hook-form";
 
-
 const NewSongForm = ({ submitSong }) => {
-  return <div></div>;
+  const { register, handleSubmit, errors } = useForm();
+  const onSubmit = (song) => submitSong(song);
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input ref={register({ required: "Song title required" })} name="title" />
+      {errors.title && <p>{errors.title.message}</p>}
+      <br></br>
+      <select name="album" ref={register}>
+        <option value="Guppy">Guppy</option>
+        <option value="Young Enough">Young Enough</option>
+        <option value="Supermoon EP">Supermoon EP</option>
+      </select>
+      <br></br>
+      <button type="submit">Submit</button>
+    </form>
+  );
 };
 
 export default NewSongForm;
